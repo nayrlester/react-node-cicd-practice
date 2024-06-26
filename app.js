600be5dbefce06f970ca9d811b9a61829fe01ab5
ryanLester <rbulot12@gmail.com>
2024-06-26 23:34:22 +0800

    configure github actions

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express")
 const app = express()
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/name', (req, res) => {
     res.send('Ryan Lester Bulot')
