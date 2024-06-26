f24f69f6b4d23154d01485f5637d1df2bfe77c02
ryanLester <rbulot12@gmail.com>
2024-06-27 00:36:47 +0800

    added jest test workflows

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+const path = require('path')
 const express = require("express")
 const app = express()
-const path = require('path')
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
